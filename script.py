f0232fd706ccf3d1aafd26ff561c6be278bb9f0e
Breno Abdala <brenoabdala@outlook.com>
2022-12-26 09:42:34 -0300

    Update: Adicionado condição para disparo de multiplos e-mails

--- script.py
+++ script.py
@@ -3,19 +3,21 @@ import smtplib
 import email.message
 
 
-def enviar_email():
-    NOME = ''
+destinatario = ['email1', 'email2', 'email3']
+
+
+def enviar_email(e_mail_do_destinatario):
 
     corpo_email = """
-  <p> Olá {NOME}, boa tarde, tudo bem? </p> 
+  <p> Olá, boa tarde, tudo bem? </p> 
   <p> Gostaria de comunica-lo através desse e-mail que o(a) senhor(a)... </p> 
   """
 
     msg = email.message.Message()
     msg['Subject'] = "Assunto"
-    msg['From'] = " e-mail de origem"
-    msg['to'] = "destinatário"
-    password = "código gerado no G-MAIL"
+    msg['From'] = ""
+    msg['to'] = e_mail_do_destinatario
+    password = ""
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email)
 
@@ -25,8 +27,8 @@ def enviar_email():
     # LOGIN CREDENDIALS FOR SENDING E-MAIL
     s.login(msg['From'], password)
     s.sendmail(msg['From'], msg['To'], msg.as_string().encode('utf-8'))
-    print('E-mail enviado')
 
 
 if __name__ == "__main__":
-    enviar_email()
+    for email_ in destinatario:
+        enviar_email(email_)
